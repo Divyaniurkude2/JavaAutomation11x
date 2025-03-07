dbe0369ae8b05dea1fa52dec3a29896782dcb475
Divyaniurkude2 <yournewemail@example.com>
2025-03-07 19:52:39 +0530

    change the folder name

--- src/ex_01_JavaBasic/HelloWorld.java
+++ src/ex_01_JavaBasic/HelloWorld.java
@@ -1,4 +1,4 @@
-package JavaBasic;
+package ex_01_JavaBasic;
 
 public class HelloWorld {
     public static void main(String[] args) {
--- src/ex_01_JavaBasic/hello.java
+++ src/ex_01_JavaBasic/hello.java
@@ -1,3 +1,5 @@
+package ex_01_JavaBasic;
+
 public class hello {
     public static void main(String[] args) {
         System.out.println("Having dinner");
