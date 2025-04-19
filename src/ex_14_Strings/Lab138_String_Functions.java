16dd2d0bb4464410795b5c1c347c1894a9c7fe4e
Divyaniurkude2 <yournewemail@example.com>
2025-04-19 14:42:58 +0530

    24th March

---
+++ src/ex_14_Strings/Lab138_String_Functions.java
@@ -0,0 +1,11 @@
+package ex_14_Strings;
+
+public class Lab138_String_Functions {
+    public static void main(String[] args) {
+        String name = "Divyani";
+        name = name.toUpperCase();
+        System.out.println(name);
+
+
+    }
+}
