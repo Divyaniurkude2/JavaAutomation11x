65d55bd4bde32f80ef0a5c9330aadd291e528d94
Divyaniurkude2 <yournewemail@example.com>
2025-04-19 19:39:25 +0530

    28th March

---
+++ src/ex_16_Arrays/Lab150_Arrays.java
@@ -0,0 +1,19 @@
+package ex_16_Arrays;
+
+public class Lab150_Arrays {
+    public static void main(String[] args) {
+        int[] marks = {1,2,3,4,5,6};
+
+        //2nd way to create the array
+        int[] marks2 = new int[5];//Fixed size
+        int mark3[] = new int[5];//Fixed size
+
+        int mark4 = 10;
+        String[] names = new String[3];
+        names[0] = "Divyani";
+        names[1] = "Ram";
+        names[2] = "Sonu";
+
+        System.out.println(marks[0]);
+    }
+}
