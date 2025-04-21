98f6812513b09bc3e62f74ca274df1aeeaf37e47
Divyaniurkude2 <yournewemail@example.com>
2025-04-21 21:48:08 +0530

    7th April

--- src/ex_25_OOPs_Abstraction_Interface/Lab198_Interface.java
+++ src/ex_25_OOPs_Abstraction_Interface/Lab198_Interface.java
@@ -1,6 +1,5 @@
-package ex_25_OOPs_Abstraction_Interface;
 
-import java.awt.*;
+package ex_25_OOPs_Abstraction_Interface;
 
 public class Lab198_Interface {
     public static void main(String[] args) {
@@ -9,5 +8,26 @@ public class Lab198_Interface {
 
         Square s1 = new Square();
         s1.getArea(3,4);
+
     }
+}
+
+class Square implements Polygon{
+
+    @Override
+    public void getArea(int length, int breadth) {
+        System.out.println("Area od Square -> "+3.14*length*breadth);
+    }
+}
+
+class Rectangle implements Polygon{
+
+    @Override
+    public void getArea(int length, int breadth) {
+        System.out.println("The area of the rectangle is " + (length * breadth));
+
+    }
+}
+interface Polygon{
+    void getArea(int length, int breadth);
 }
